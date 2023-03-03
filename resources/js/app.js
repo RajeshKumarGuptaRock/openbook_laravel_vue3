3dde210003d5f2b05999e6859eaba69c7ee276e8
rajesh <developerrajeshgupta@gmail.com>
2023-03-03 22:11:21 +0530

    Add vue route for pages  and make login,registration api

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 import './bootstrap';
 import { createApp } from 'vue'
 import app from './layouts/app.vue'
-
+import router from './router.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
-createApp(app).use(bootstrap).mount("#app")
+createApp(app).use(bootstrap).use(router).mount("#app")
--- resources/js/layouts/app.vue
+++ resources/js/layouts/app.vue
@@ -1,27 +1,14 @@
 <template>
-    <div> <h1>laravel 9</h1></div>
-    <div class="container">
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="https://picsum.photos/id/870/600/300" class="d-block w-100" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="https://picsum.photos/id/871/600/300" class="d-block w-100" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="https://picsum.photos/id/872/600/300" class="d-block w-100" alt="...">
-              </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
-          </div>
-    </div>
-
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <router-link class="text-white ml-2" :to="{name :'Home' }">Home</router-link>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="text-white ml-2" to="/login">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="text-white ml-2" to="/register">Register</router-link>
+          </li>
+        </ul>        
+      </nav>
+      <router-view></router-view>
 </template>
